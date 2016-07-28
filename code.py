c70b00c85b58e63d42764733996becd6f11368bd
Simon Gawlik <sgawlik@gmail.com>
2016-07-28 18:09:43 -0400

    first commit

---
+++ code.py
@@ -0,0 +1,25 @@
+import web
+
+render = web.template.render('templates/')
+
+db = web.database(dbn='postgres', user='username', pw='password', db='trywebpy')
+
+urls = (
+	'/', 'index',
+	'/add', 'add'
+)
+
+class index:
+	def GET(self):
+		todos = db.select('todo')
+		return render.index(todos)
+
+class add:
+	def POST(self):
+		i = web.input()
+		n = db.insert('todo', title=i.title)
+		raise web.seeother('/')
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
